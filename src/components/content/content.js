0ce3f1169d6613e562a8e5e1f600a43ce7e5f81a
st33ze <paweltrojnara@gmail.com>
2024-12-20 10:31:41 +0100

    feat: add main content component

---
+++ src/components/content/content.js
@@ -0,0 +1,12 @@
+import './content.css';
+
+export default class Content {
+  #node;
+
+  constructor() {
+    this.#node = document.createElement('main');
+  }
+
+  render(parent, pageName) {
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import './main.css';
 
 import { header } from './components/header/header.js';
+import Content from './components/content/content.js';
 
 const app = document.createElement('div');
 app.id = 'app';
 document.body.appendChild(app);
 
 app.appendChild(header);
+
+const content = new Content();
+content.render(app, 'today'); // Render starting page
