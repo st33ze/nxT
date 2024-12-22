e971b590be97769ac610a4d86c7d9da07a34a4bc
st33ze <paweltrojnara@gmail.com>
2024-12-22 11:10:29 +0100

    refactor(button): add iconString and className to constructor
    Make the component more modular and usable in other scenarios.

--- src/components/common/button.js
+++ src/components/common/button.js
@@ -3,15 +3,16 @@ import './button.css';
 export default class Button {
   #node
 
-  constructor(event) {
+  constructor(iconString=null, className='', event) {
     this.#node = document.createElement('button');
-    this.#node.classList.add('add-btn');
+    this.#node.classList.add(className);
+    this.iconString = iconString;
     this.event = event;  
   }
 
   #createIcon() {
     const container = document.createElement('div');
-    container.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M18 13h-5v5c0 .55-.45 1-1 1s-1-.45-1-1v-5H6c-.55 0-1-.45-1-1s.45-1 1-1h5V6c0-.55.45-1 1-1s1 .45 1 1v5h5c.55 0 1 .45 1 1s-.45 1-1 1"/></svg>`;
+    container.innerHTML = this.iconString;
 
     const icon = container.querySelector('svg');
     icon.setAttribute('aria-hidden', 'true');
@@ -19,6 +20,7 @@ export default class Button {
 
     return icon;
   }
+  
   render() {
     this.#node.appendChild(this.#createIcon());
 
