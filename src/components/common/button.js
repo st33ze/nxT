955bea7e4441af20af68787ec2898169ea5aac5c
st33ze <paweltrojnara@gmail.com>
2024-12-25 16:26:42 +0100

    refactor(button): change to enable button with either text or icon

--- src/components/common/button.js
+++ src/components/common/button.js
@@ -3,27 +3,44 @@ import './button.css';
 export default class Button {
   #node
 
-  constructor(iconString=null, className='', event) {
+  /**
+   * Constructor for the Button class.
+   * 
+   * @param {string} label - The accessible label for the button.
+   * @param {string|null} iconString - An optional SVG string representing the button's icon.
+   */
+  constructor(label, iconString=null) {
     this.#node = document.createElement('button');
-    this.#node.classList.add(className);
-    this.iconString = iconString;
-    this.event = event;  
-  }
+    this.#node.classList.add(`${label}-btn`);
 
-  #createIcon() {
-    const container = document.createElement('div');
-    container.innerHTML = this.iconString;
-
-    const icon = container.querySelector('svg');
-    icon.setAttribute('aria-hidden', 'true');
-    icon.setAttribute('focusable', 'false');
-
-    return icon;
+    // Append either the icon with an aria-label or a text node
+    if (iconString) {
+      this.#node.appendChild(this.#createIcon(iconString));
+      this.#node.setAttribute('aria-label', label);
+    } else this.#node.appendChild(document.createTextNode(label));
+    
   }
   
-  render() {
-    this.#node.appendChild(this.#createIcon());
+  #createIcon(iconString) {
+    const container = document.createElement('div');
+    container.innerHTML = iconString;
+    
+    const icon = container.querySelector('svg');
+    if (!icon) 
+        throw new Error('Invalid SVG string: no <svg> element found.');
+    
+    icon.setAttribute('aria-hidden', 'true');
+    icon.setAttribute('focusable', 'false');
+    
+    return icon;
+  }
 
+  /**
+   * Getter for the button DOM node.
+   * 
+   * @returns {HTMLButtonElement} - The DOM node for the button.
+   */
+  get node() {
     return this.#node;
   }
 }
