1d46e2c6d23c1fb8e51605271973517c58bb97a0
st33ze <paweltrojnara@gmail.com>
2025-01-13 10:56:51 +0100

    feat(today): add modal components and functionality

--- src/pages/today.js
+++ src/pages/today.js
@@ -1,19 +1,27 @@
 import './today.css';
-import Button from '../components/common/button.js';
-import icons from '../assets/icons.js';
+import Modal from '../components/modals/modal.js';
+import TaskModal from '../components/modals/taskModal.js';
+import { createNode } from '../utils/domUtils.js';
+import { createSVGElement } from '../assets/icons.js';
+import bus, { EVENTS } from '../utils/bus.js';
 
 export default class Today {
   #node
+  #modal
+  #newTaskBtn
 
   constructor() {
-    // Main container for the page
-    this.#node = document.createElement('div');
-    this.#node.classList.add('page-today');
+    this.#node = createNode('div', {'class': 'page-today'});
 
     const header = this.#createHeader();
-    const addTaskBtn = new Button('add', icons.add);
+    this.#newTaskBtn = this.#createNewTaskBtn();
 
-    this.#node.append(header, addTaskBtn.node);
+    const modalContent = new TaskModal();
+    this.#modal = new Modal(modalContent.node);
+
+    this.#addEventListeners();
+
+    this.#node.append(header, this.#newTaskBtn, this.#modal.node);
   }
   
   #createHeader() {
@@ -25,6 +33,36 @@ export default class Today {
     return header;
   }
 
+  #createNewTaskBtn() {
+    const button = createNode('button', {
+      'class': 'add-btn',
+      'aria-label': 'Add a task',
+    });
+    button.appendChild(createSVGElement('add'));
+
+    button.addEventListener('click', () => {
+      this.#modal.open();
+      this.#newTaskBtn.setAttribute('disabled', 'true');
+    });
+
+    return button;
+  }
+  
+  #activateNewTaskBtn() {
+    this.#newTaskBtn.removeAttribute('disabled');
+    this.#newTaskBtn.focus();
+  }
+
+  #addEventListeners() {
+    bus.clear(EVENTS.MODAL_CLOSE, EVENTS.MODAL_SAVE);
+    bus.on(EVENTS.MODAL_CLOSE, () => this.#activateNewTaskBtn());
+    bus.on(EVENTS.MODAL_SAVE, (task) => {
+      console.log(task);
+      this.#modal.close();
+      this.#activateNewTaskBtn();
+    })
+
+  }
   /**
    * Getter for the root DOM node of the "Today" page.
    * @returns {HTMLElement} The root DOM node.
