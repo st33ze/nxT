f1dc4ad1bcc738e8448cff6c292b76e3b64e7b07
st33ze <paweltrojnara@gmail.com>
2025-03-04 12:49:28 +0100

    feat(projects): open modal for new project in project page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -3,11 +3,50 @@ import { createNode } from '../utils/domUtils.js';
 import AddButton from '../components/common/addButton.js';
 import db from '../utils/dbManager.js';
 import ProjectList from '../components/common/projectList.js';
+import bus, {EVENTS} from '../utils/bus.js';
+import Modal from '../components/modals/modal.js';
+import ProjectModal from '../components/modals/projectModal.js';
+
+const testProject = {
+  id: 1,
+  title: "To ensure that the title is vertically centered and to add ellipsis",
+  description: "A software development project for a client in the healthcare industry. The project involves creating a web application for managing patient records, appointments, and billing.",
+  tasks: [
+    {
+      id: 1,
+      title: "Finish project report",
+      description: "Complete the final detailed report for the ABC project, which includes financial data, project milestones, and lessons learned. Ensure the report is formatted properly and reviewed before submission to the management team.",
+      date: "2025-01-24",
+      priority: "high",
+      completed: false,
+      projectId: 1,
+    },
+    {
+      id: 2,
+      title: "Grocery shopping",
+      description: "Go to the local supermarket to buy fresh vegetables, seasonal fruits, dairy products, and other weekly essentials. Also, check for discounts on household items like cleaning supplies.",
+      date: "2025-01-25",
+      priority: "medium",
+      completed: true,
+      projectId: 1,
+    },
+    {
+      id: 3,
+      title: "Call with the client",
+      description: "Set up a one-hour video call with the client to discuss progress on the project milestones. Prepare a brief update presentation, including timelines, current challenges, and proposed solutions.",
+      date: "2025-01-26",
+      priority: "high",
+      completed: false,
+      projectId: 1,
+    },
+  ]
+}
 
 
 export default class Projects {
   #node;
   #projectList;
+  #modal
 
   constructor() {
     this.#node = createNode('div', {'class': 'page-projects'});
@@ -17,7 +56,12 @@ export default class Projects {
     const newProjectBtn = this.#createNewProjectBtn().node;
     pageContent.append(header, newProjectBtn);
 
-    this.#node.appendChild(pageContent);
+    this.#modal = {
+      window: new Modal(this.#onModalClose.bind(this)),
+      content: new ProjectModal(),
+    }
+
+    this.#node.append(pageContent, this.#modal.window.node);
 
     this.#loadProjectsFromDB().then((projects) => {
       this.#projectList = new ProjectList(projects);
@@ -37,9 +81,25 @@ export default class Projects {
     return header;
   }
 
+  #openModal(project={}) {
+    this.#modal.content.render(project);
+    console.log(this.#modal.content);
+    bus.emit(EVENTS.MODAL.OPEN, this.#modal.content.node);
+    this.#node.querySelector('.projects-content').setAttribute('inert', '');
+  }
+
   #createNewProjectBtn() {
     const button = new AddButton();
+    button.label = 'Create new project';
+    button.addEventListener('click', () => this.#openModal());
+
     return button;
+  }
+
+  #onModalClose() {
+    const content = this.#node.querySelector('.projects-content');
+    content.removeAttribute('inert');
+    content.querySelector('.add-btn').focus();
   }
 
   async #loadProjectsFromDB() {
