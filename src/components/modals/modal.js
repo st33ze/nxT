663ef35848c2dca4192a101c0434f01129052ec4
st33ze <paweltrojnara@gmail.com>
2025-03-13 10:51:10 +0100

    refactor(modal): remove overlay element and display it as pseudo element in css

--- src/components/modals/modal.js
+++ src/components/modals/modal.js
@@ -12,7 +12,10 @@ export default class Modal {
       'aria-hidden': 'true',
     });
     
-    this.#init();
+    this.#node.append(
+      this.#createCloseBtn(),
+      createNode('div', {class: 'modal-content'})
+    );
 
     this.#addEventListeners();
   }
@@ -28,38 +31,27 @@ export default class Modal {
 
     return btn;
   }
-  
-  #init() {
-    const modalWindow = createNode('div', {class: 'modal-window'});
 
-    modalWindow.append(
-      this.#createCloseBtn(),
-      createNode('div', {class: 'modal-content'})
-    );
-    
-    this.#node.appendChild(modalWindow);
-  }
-  
   #open(contentNode) {
     this.#node.querySelector('.modal-content')
       .appendChild(contentNode);
 
-    this.#node.classList.add('modal-open');
+    this.#node.classList.add('open');
     this.#node.setAttribute('aria-hidden', 'false');
     
     this.#node.querySelector('.close-btn').focus();
   }
   
   #close() {
-    this.#node.classList.add('modal-closing');
+    this.#node.classList.add('closing');
     this.#node.setAttribute('aria-hidden', 'true');
     
     setTimeout(() => {
-      this.#node.classList.remove('modal-open', 'modal-closing');
+      this.#node.classList.remove('open', 'closing');
       this.#node.querySelector('.modal-content').innerHTML = '';
     }, 500);
   }
-  
+
   #addEventListeners() {
     bus.on(EVENTS.MODAL.OPEN, (content) => this.#open(content), {clearOnReload: true});
     bus.on(EVENTS.MODAL.CLOSE, () => this.#close(), {clearOnReload: true});
