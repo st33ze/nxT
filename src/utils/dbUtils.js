fad18c8f95f98f060151ada984187c4938e0597e
st33ze <paweltrojnara@gmail.com>
2025-09-24 14:15:40 +0200

    feat(dbUtils): add promisifyRequest() helper

---
+++ src/utils/dbUtils.js
@@ -0,0 +1,6 @@
+export function promisifyRequest(req) {
+  return new Promise((resolve, reject) => {
+    req.onsuccess = () => resolve(req.result);
+    req.onerror = () => reject(req.error);
+  });
+}
