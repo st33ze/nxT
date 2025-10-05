60627fc95ac593199539556809164d94b861bb4f
st33ze <paweltrojnara@gmail.com>
2025-10-05 12:26:41 +0200

    feat(dbUtils): transaction helper function

--- src/utils/dbUtils.js
+++ src/utils/dbUtils.js
@@ -4,3 +4,11 @@ export function promisifyRequest(req) {
     req.onerror = () => reject(req.error);
   });
 }
+
+export function transactionPromise(transaction) {
+  return new Promise((resolve, reject) => {
+    transaction.oncomplete = () => resolve();
+    transaction.onerror = e => reject(e);
+    transaction.onabort = e => reject(e);
+  });
+}
